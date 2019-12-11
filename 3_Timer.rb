aaafcdf1b8657b7de7f828ce82240bf7055ff3d2
shicca.rorl <shicca.rorl@gmail.com>
2019-12-11 15:47:28 +0900

    2019-12-11 夕方

--- 3_Timer.rb
+++ 3_Timer.rb
@@ -1,36 +1,29 @@
 # -*- encoding: utf-8 -*-
 
-require 'rss'
-require "date"
 
-require 'sequel'
 require './3_Config'
 
 
-class Create_Rss
-  def initialize()
-    @config = Config.new()
+class Timer
+  # 分刻み
+  def initialize(time)
+    @time = time
   end
-  def get_rdf()
-    rss = RSS::Maker.make("1.0") do |maker|
-      maker.channel.about = @config.top_home_page + "index.rss"
-      maker.channel.title = @config.home_title
-      maker.channel.description = @config.home_description[0]
-      maker.channel.link = @config.top_home_page
-
-      @db[:Article].order(:article_date).limit(20).all.each{|report|
-        maker.items.new_item do |item|
-          item.link = report[:article_link]
-          item.title = report[:article_title]
-          item.pubDate = report[:article_date]
-        end
-      }
+  def get_time()
+    time2 = Time.now()
+    if (Time.now() - time2) >= @time then
+      return true
+    else
+      return false
     end
-    File.open(@config.www_html_out_path + "index.rss", "wb:utf-8") do |f|
-      f.write(rss)
-    end
+    p (Time.now() - time2)
+    return false
   end
 end
 
-#@affilete_RssReadr_Create_Rss = Create_Rss.new()
-#@affilete_RssReadr_Create_Rss.get_rdf()
+
+time = Timer.new(10)
+loop do
+  p time.get_time()
+  sleep (1)
+end
---
+++ Affiliate_RssReadr_3.rb
@@ -0,0 +1,177 @@
+# -*- encoding: utf-8 -*-
+
+require "erb"
+require "date"
+require 'sequel'
+
+require './3_Config'
+#require './3_kyou_no_yome'
+
+class CreateHtml
+  attr_accessor :keyword, :css_theme_path, :config, :report
+  def initialize()
+    @config = Config.new()
+
+    @header          = File.open("./template/header.erb", 'r:utf-8').read
+    @footer          = File.open("./template/footer.erb", 'r:utf-8').read
+    @body            = File.open("./template/body.erb", 'r:utf-8').read
+    @body_yome       = File.open("./template/body_yome.erb", 'r:utf-8').read
+    @autoupload_lftp = File.open("./autoupload.lftp", 'r:utf-8').read
+
+    @title = ""
+
+    @page_no = 0
+
+    self.keyword()
+
+  end
+
+  def keyword()
+    @keyword = ""
+    if @config.home_category == nil then
+      @keyword = "err"
+    elsif @config.home_category != nil then
+      @config.home_category.each{|key|
+        @keyword = "#{@keyword}, " + "#{key[0]}"
+      }
+    end
+    return @keyword.encode!("UTF-8")
+  end
+
+  def create_category()
+    #何かオプションを指定する場合は下記に追記する
+    #options = {:encoding=>"utf8"}
+
+    #@dbに接続
+    #@db = Sequel.sqlite("RssData.SQLite3", options)
+
+    @config.home_category.each{|x, y|
+      @report = @config.db[:news].where(Sequel.ilike(:title, "%#{ x }%")).limit(100).order(Sequel.desc(:published)).all
+      @html = @header + @body + @footer
+
+      #p changelogmemo
+      erb = ERB.new(@html)
+      @html = erb.result(binding)
+      begin
+        File.write(@config.www_html_out_path + "#{y}.html", @html)
+      rescue
+        print "カテゴリー　書き込みエラー\n"
+      end
+    }
+  end
+
+  def create_body()
+    #何かオプションを指定する場合は下記に追記する
+    #options = {:encoding=>"utf8"}
+
+    #@dbに接続
+    #@db = Sequel.sqlite("RssData.SQLite3", options)
+    #@config = Config.new()
+
+    @page_end = @config.db[:news].all.size / @config.limit
+
+    @page_no = 0
+    start = @config.limit * @page_no
+
+    @page_no = 0
+    (1..@page_end).each{|damy|
+      @report = @config.db[:news].limit(@config.limit).offset(start).order(Sequel.desc(:published)).all
+
+      start = @config.limit * @page_no
+
+      # くっつける
+      @html = @header + @body + @footer
+      #p changelogmemo
+
+      erb = ERB.new(@html)
+
+      @html = erb.result(binding)
+
+      #print "Report 最大 " + @config.db[:news].all.size.to_s + "\n"
+      if @page_no == 0 then
+        begin
+          File.write(@config.www_html_out_path + "index.html", @html)
+        rescue
+          print "index 書き込みエラー\n"
+        end
+      else
+        begin
+          #print "ページ  #{@page_no}  作成\n"
+          File.write(@config.www_html_out_path + "index#{@page_no}.html", @html)
+        rescue
+          print "subpage 書き込みエラー\n"
+        end
+      end
+      @page_no = @page_no + 1
+    }
+
+    @page_end = @page_no
+  end
+
+  def create_body_yome()
+    #何かオプションを指定する場合は下記に追記する
+    #options = {:encoding=>"utf8"}
+
+    #@dbに接続
+    #@db = Sequel.sqlite("RssData.SQLite3", options)
+    #@config = Config.new()
+
+    #@affiliate = @config.my_db[:affiliate].all
+    #@affiliate_DMM = @db[:affiliate_DMM].all
+    #@affiliate_DMM_Video = @db[:affiliate_DMM_Video].all
+
+
+    @date = Date.today
+    if @config.my_db[:Article].where(:url=>@config.web_pic + "#{@date.to_s}.html").first == [] then
+      @page_end = @config.db[:news].all.size / @config.limit
+      @pic = @config.my_db[:pic].all
+
+      # くっつける
+      @html = @header + @body_yome + @footer
+      #p changelogmemo
+
+      erb = ERB.new(@html)
+
+      @html = erb.result(binding)
+
+      @config.my_db[:Article].insert(:article_title=>"#{@date.to_s} 今日の嫁", :article_link=>@config.web_pic + "#{@date.to_s}.html", :article_date=>@date, :use_time=>Time.now)
+      
+      begin
+        File.write(@config.web_pic + "#{@date.to_s}.html", @html)
+      rescue
+        print "書き込みエラー\n"
+      end
+    
+    else
+      print "今日は何回するき？\n"
+    end
+  end
+
+
+
+  def lftp()
+    erb = ERB.new(@autoupload_lftp)
+    lftp = erb.result(binding)
+
+    begin
+      File.write("./autoupload.lftp", lftp)
+    rescue
+      p "書き込みエラー"
+    end
+
+    command = "lftp -f ./autoupload.lftp"
+    begin
+      system(command)
+    rescue
+      print "アップロード失敗\n"
+    end
+
+  end
+end
+
+
+create_html = CreateHtml.new()
+create_html.create_category()
+create_html.create_body()
+#create_html.create_body_yome()
+create_html.lftp()

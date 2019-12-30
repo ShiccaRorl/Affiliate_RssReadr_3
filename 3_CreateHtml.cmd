331fd4d7889e706477aca311ffa62b19c437bb90
shicca.rorl <shicca.rorl@gmail.com>
2019-12-30 11:06:15 +0900

    syuusei

--- 3_CreateHtml.cmd
+++ 3_CreateHtml.cmd
@@ -1,5 +1,5 @@
-rem call ruby 3_CreateHtml.rb
+call ruby 3_CreateHtml.rb
 
-call curl -# -T e:\\www\index.html -u kyounoyome:X?GF1ytvn@ --ftp-create-dirs -ssl -ftp-ssl -ftp-pasv ftp:\\kyounoyome.x.fc2.com\
-     curl -# -T e://www/index.html -u kyounoyome:X?GF1ytvn@ -w "%{url_effective}:%{http_code}\n" --ftp-create-dirs -ftp-ssl -ftp-pasv ftp://kyounoyome.x.fc2.com/
+rem call curl -# -T e:\\www\index.html -u kyounoyome:X?GF1ytvn@ --ftp-create-dirs -ssl -ftp-ssl -ftp-pasv ftp:\\kyounoyome.x.fc2.com\
+    call curl -# -T e://www/index.html -u kyounoyome:X?GF1ytvn@ -w "%{url_effective}:%{http_code}\n" --ftp-create-dirs -ftp-ssl -ftp-pasv ftp://kyounoyome.x.fc2.com/
 pause

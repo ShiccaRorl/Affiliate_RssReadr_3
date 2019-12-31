0b727f4f74b6ead4eb8d8100eb0365e0c30a28ce
shicca.rorl <shicca.rorl@gmail.com>
2019-12-31 15:40:28 +0900

    変更前

--- A_TimeApp.py
+++ A_TimeApp.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-#from A_News import News
+from A_News import News
 from A_QuiteRSS import QuiteRSS
 import datetime
 from sqlalchemy import desc
@@ -19,51 +19,40 @@ def dateTimeApp():
     for row2 in session2.query(QuiteRSS).order_by(desc(News.received)):
         #print(row2.id, row2.feedId, row2.title, row2.published, row2.received, row2.link_href)
         #print(row2.published)
-        time.sleep(1)
+        #time.sleep(1)
         i = 0
         if i <= 20:
-            try:
-                newss = session1.query(News.id).order_by(desc(News.received)).all()
+            #try:
+                newss = session2.query(News.id).order_by(desc(News.received)).all()
                 for news in newss:
 
     
                     print(row2.title)
-                    print(row2.published.replace("T", " "))
                     print(row2.published.replace("T", " "), '%Y-%m-%d %H:%M:%S')
                     time1 = datetime.datetime.strptime(row2.published.replace("T", " "), '%Y-%m-%d %H:%M:%S')
-                    time2 = datetime.datetime.strptime(row2.received.replace("T", " "), '%Y-%m-%d %H:%M:%S')
-
         
                     #ed_user = News(row.id==row2.id, row.feedId==row2.feedId, row.title==row2.title, row.published==time1, row.received==time2, row.link_href==row2.link_href)
                     #session1.add(ed_user)
 
-                    published = session.query(News).filter(User.id==none).first()
-                    published.published = time1
+                    news = session2.query(News.id).filter(News.published_datetime == (None)).first()
+
+                    print("time1 " + str(time1) + "\n")
+                    print(str(news) + str(news) + "\n")
+                    news.published_datetime = time1
                     session2.commit()
 
                     print ("データ取得")
-            except:
+            #except:
                     print ("不思議なエラー")
                     # 不思議エラーが20回続くと終わる。
                     print ("不思議エラー " + str(i) + " 回目")
                     i = i + 1
-                    if i >= 20:
-                        break
+                    #if i >= 20:
+                        #break
 
         
     # セッション・クローズ
     # DB処理が不要になったタイミングやスクリプトの最後で実行
     session2.close()
 
-
-
-
-
-
 dateTimeApp()
-
-
-
-
-
-

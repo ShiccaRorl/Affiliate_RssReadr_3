8ae661bb527b001a14384b698e050fc7e60bab07
ban <shicca.rorl@gmail.com>
2020-01-10 07:24:12 +0900

    同じメッセージを消します。日付を取得しているところまで。

---
+++ A_Same_data_deletion.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from A_News import News
+from A_QuiteRSS import QuiteRSS
+import datetime
+from sqlalchemy import desc
+
+from time import sleep
+
+class SameDataDeletion():
+    def session_open(self):
+        #engine2 = create_engine('sqlite:///home/ban/.local/share/QuiteRss/QuiteRss/feeds.db', echo=False)
+        engine2 = create_engine('sqlite:///C:/Users/ban/AppData/Local/QuiteRss/QuiteRss/feeds.db', echo=False)
+        Session2 = sessionmaker(bind=engine2)
+        self.session2 = Session2()
+
+
+    def session_close(self):
+        # セッション・クローズ
+        # DB処理が不要になったタイミングやスクリプトの最後で実行
+
+        self.session2.close()
+
+    def timeInput(self):
+        i = 0
+        self.session_open() # .filter(News.id==i, News.published_datetime==None)
+        for row2 in self.session2.query(News).filter(News.published_datetime==None).order_by(News.id).all(): # 全データ指定
+            try:
+                print(row2.title)
+                time1 = datetime.datetime.strptime(row2.published.replace("T", " "), '%Y-%m-%d %H:%M:%S')
+                print(time1)
+                #article = self.session2.query(News).filter(row2.id==i, row2.published_datetime==None).first()
+
+                row2.published_datetime = time1
+
+                self.session2.commit()
+                print ("データ追加")
+                print(str(row2.id))
+                i = i + 1
+            except:
+                print ("データがあります。")
+                print(str(row2.id))
+            
+            sleep(1)
+        self.session_close()
+        print("取得データ" + str(i) + "個目")
+
+    def delete(self):
+        print ("")
+
+
+if __name__ == '__main__':
+    sameData = SameDataDeletion()
+
+    import sys
+
+    args = sys.argv
+    if args[1] == None:
+        print ("オプション time 日付データを追加します。")
+        print ("オプション del 同じ記事を削除します。新しいのが残るようにします。")
+    elif str(args[1]) == "time":
+        sameData.timeInput()
+    elif args[1] == "del":
+        sameData.delete()
+
+    else:
+        print ("オプション time 日付データを追加します。")
+        print ("オプション del 同じ記事を削除します。新しいのが残るようにします。")

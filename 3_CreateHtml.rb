6160230ceb1d1c520578c4340cbfce0b9d1ca202
ban <shicca.rorl@gmail.com>
2020-01-09 20:05:27 +0900

    test

--- 3_CreateHtml.rb
+++ 3_CreateHtml.rb
@@ -1,13 +1,10 @@
-#! snap/ruby/166/bin/ruby
 # -*- encoding: utf-8 -*-
 
 require "erb"
 require "date"
-require "sqlite3"
-require "sequel"
+require 'sequel'
 
-
-require './3_Config'
+require  './3_Config'
 #require './3_kyou_no_yome'
 
 class CreateHtml
@@ -184,17 +181,16 @@ class CreateHtml
   end
 
   def html_up()
-    File.open("upload.cmd", "w") do |f|    
+    #File.open("upload.cmd", "w") do |f|    
       Dir.glob("#{@config.www_html_out_path}**/*.html").each{|file|
-        f.puts("curl -# -T #{file} -u #{@config.ftp_user}:#{@config.ftp_pass} -w %{url_effective}:%{http_code} --ftp-create-dirs -ftp-ssl -ftp-pasv ftp://#{@config.ftp_server}/")
-        f.puts("timeout /t 5 > nul")
+        #f.puts("curl -# -T #{file} -u #{@config.ftp_user}:#{@config.ftp_pass} -w %{url_effective}:%{http_code} --ftp-create-dirs -ftp-ssl -ftp-pasv ftp://#{@config.ftp_server}/")
+        #f.puts("timeout /t 5 > nul")
         system("curl -# -T #{file} -u #{@config.ftp_user}:#{@config.ftp_pass} -w %{url_effective}:%{http_code} --ftp-create-dirs -ftp-ssl -ftp-pasv ftp://#{@config.ftp_server}/")
-	sleep(1)
     }
     end
     #system("upload.cmd")
   end
-end
+
 
 
 create_html = CreateHtml.new()
